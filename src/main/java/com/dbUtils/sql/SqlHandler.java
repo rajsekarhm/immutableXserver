a5c1bb0e001571c8102158f2605c00d591b58e09
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-08-25 10:46:01 +0530

    statement code changes

--- src/main/java/com/dbUtils/sql/SqlHandler.java
+++ src/main/java/com/dbUtils/sql/SqlHandler.java
@@ -1,13 +1,17 @@
 package com.dbUtils.sql;
 
+import org.apache.kafka.common.protocol.types.Field;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SqlHandler implements ISqlHandler{
     private  String url;
     private  String username;
     private  String password;
+    Statement statement;
 
     public  void setDBUtilsProperty(String url,String username,String password){
         this.url = url;
@@ -16,5 +20,12 @@ public class SqlHandler implements ISqlHandler{
     }
     public Connection getConnection() throws SQLException {
          return DriverManager.getConnection(this.url,this.username,this.password);
+    }
+    public Statement createStatement() throws SQLException {
+        return getConnection().createStatement();
+    }
+
+    public Statement prepareStatement(String query) throws SQLException {
+        return  getConnection().prepareStatement(query);
     }
 }
