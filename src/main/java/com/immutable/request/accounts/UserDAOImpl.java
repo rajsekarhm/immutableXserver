e8f4db738d18abebf6d475df03ae25c1fc65bacc
rajasekar.hm <rajasekar.hm@zohocorp.com>
2024-10-31 16:07:03 +0530

    redis store impl

--- src/main/java/com/immutable/request/accounts/UserDAOImpl.java
+++ src/main/java/com/immutable/request/accounts/UserDAOImpl.java
@@ -1,13 +1,19 @@
 package com.immutable.request.accounts;
 
+import com.dependencies.jedis.IJedis;
+import com.dependencies.jedis.JedisImx;
+import com.google.gson.Gson;
 import com.immutable.authentication.AuthenticationUser;
 import com.immutable.authentication.IAuthentication;
 import com.immutable.authorization.AuthorizationUser;
 import com.immutable.authorization.IAuthorization;
 import com.immutable.request.entities.Entities;
 import com.immutable.request.entities.UserDB_Handlers;
+import com.immutable.request.utils.Formatter;
 import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 
+import java.io.IOException;
 import java.util.Objects;
 
 @RestController
@@ -20,20 +26,20 @@ public class UserDAOImpl {
     }
 
     @RequestMapping("/createUser")
-    public String createUser(@RequestBody  UserDAO.Builder user) {
-        UserDAO u = new UserDAO();
-        System.out.println(u.toString());
-        UserDB_Handlers userBD = new UserDB_Handlers();
-        userBD.createEntities();
+    public String createUser(@RequestBody  UserDAO.Builder user) throws IOException {
+        IJedis jedis = new JedisImx();
+        jedis.setByString(user.governmentID.toString(), Formatter.toJSON(user));
         return "ok";
     }
 
     @PutMapping("/updateUser")
     public String updateUser(@RequestBody UserDAO.Builder user) {
-        UserDAO u = new UserDAO();
-        System.out.println(u.toString());
-        UserDB_Handlers userBD = new UserDB_Handlers();
-        userBD.updateEntities();
+//        UserDAO u = new UserDAO();
+//        System.out.println(u.toString());
+//        UserDB_Handlers userBD = new UserDB_Handlers();
+//        userBD.updateEntities();
+        IJedis jedis = new JedisImx();
+        jedis.setByString(user.governmentID.toString(), Formatter.toJSON(user));
         return "ok";
     }
 
@@ -45,11 +51,12 @@ public class UserDAOImpl {
     }
 
     @GetMapping("/getUser")
-    public String getUser(@RequestBody Long queryId) {
-         UserDAO u = new UserDAO();
-        System.out.println( u.toString());
-        UserDB_Handlers userBD = new UserDB_Handlers();
-        userBD.getEntities();
-        return "ok";
+    public String getUser(@RequestParam String queryId) {
+//        UserDAO u = new UserDAO();
+//        System.out.println( u.toString());
+//        UserDB_Handlers userBD = new UserDB_Handlers();
+//        userBD.getEntities(queryId);
+        IJedis jedis = new JedisImx();
+        return jedis.getByString(queryId);
     }
 }
